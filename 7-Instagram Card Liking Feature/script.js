56dcbb0147a8527d424c3a365e507859f70945de
abdcantcode <abdullah72ftw@gmail.com>
2025-12-07 21:51:50 +0530

    bgn ka kaam khatam

--- 7-Instagram Card Liking Feature/script.js
+++ 7-Instagram Card Liking Feature/script.js
@@ -1,21 +1,21 @@
-let card=document.querySelector('.card');
-let love=document.querySelector('i');
+let card = document.querySelector('.card');
+let love = document.querySelector('i');
 
-card.addEventListener('dblclick',function(){
-  love.style.opacity=1;
-  love.style.transform= 'translate(-50%, -50%) scale(1) rotate(0deg)' ;
+card.addEventListener('dblclick', function () {
+  love.style.opacity = 1;
+  love.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)';
 
-  setTimeout(function(){
-    love.style.transform= 'translate(-50%, -350%) scale(1) rotate(30deg)' ;
-  },600);
-  setTimeout(function(){
-    
-    love.style.opacity=0;
-  },800);
-  setTimeout(function(){
-    love.style.transform= 'translate(-50%, -50%) scale(1) rotate(-30deg)' ;
-    
-  },1000);
+  setTimeout(function () {
+    love.style.transform = 'translate(-50%, -350%) scale(1) rotate(30deg)';
+  }, 600);
+
+  setTimeout(function () {
+    love.style.opacity = 0;
+  }, 800);
+  setTimeout(function () {
+    love.style.transform = 'translate(-50%, -50%) scale(1) rotate(-30deg)';
+
+  }, 1000);
 
 
 })

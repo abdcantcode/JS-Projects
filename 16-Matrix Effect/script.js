7c01094231d112b2d16df260a1e6ec1ea3c6e39e
abdcantcode <abdullah72ftw@gmail.com>
2025-12-11 00:22:03 +0530

    FUNCTION BANAYA

--- 16-Matrix Effect/script.js
+++ 16-Matrix Effect/script.js
@@ -1,12 +1,15 @@
 let p=document.querySelector('p');
 let paratext=p.innerHTML;
 let random='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-setInterval(()=>{
+ 
+function randombanana(){
+  setInterval(()=>{
   const str=paratext.split('').map(()=>{
   return random.split('')[Math.floor(Math.random()*52)]
 }).join('')
 
 // console.log(str);
 },30);
+}
+
 

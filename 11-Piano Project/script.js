4c8d8d7dc4e784bd99d248a9b5fc8b2d03ffa371
abdcantcode <abdullah72ftw@gmail.com>
2025-12-11 03:00:30 +0530

    NAI HUWA THA

---
+++ 11-Piano Project/script.js
@@ -0,0 +1,8 @@
+//I have to learn the id ka cheez and implement here in sha allah 
+let audio1=new Audio(28);
+let body=document.querySelector('body');
+body.addEventListener('keydown',function(dets){
+  if(dets.key==='k'){
+    
+  }
+})

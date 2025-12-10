32fb52a951f3035070cc12f6178a9876bf8b2071
abdcantcode <abdullah72ftw@gmail.com>
2025-12-11 00:20:23 +0530

    GITHUB

---
+++ 16-Matrix Effect/script.js
@@ -0,0 +1,12 @@
+let p=document.querySelector('p');
+let paratext=p.innerHTML;
+let random='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+setInterval(()=>{
+  const str=paratext.split('').map(()=>{
+  return random.split('')[Math.floor(Math.random()*52)]
+}).join('')
+
+// console.log(str);
+},30);
+

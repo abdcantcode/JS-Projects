3ff75ea90ce6bcb6db3d0ba364709c403a1356e4
abdcantcode <abdullah72ftw@gmail.com>
2025-12-11 00:33:49 +0530

    DONE BUT DOUBTS HAI RE BABA

--- 16-Matrix Effect/script.js
+++ 16-Matrix Effect/script.js
@@ -1,15 +1,26 @@
-let p=document.querySelector('p');
-let paratext=p.innerHTML;
-let random='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
- 
-function randombanana(){
-  setInterval(()=>{
-  const str=paratext.split('').map(()=>{
-  return random.split('')[Math.floor(Math.random()*52)]
-}).join('')
+let p = document.querySelector('p');
+let paratext = p.innerHTML;
+let random = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-// console.log(str);
-},30);
+let iteration = 0;
+
+
+function randombanana() {
+
+  const str = paratext.split('').map((char, index) => {
+    if (index < iteration) {
+      return char;
+    }
+    return random.split('')[Math.floor(Math.random() * 52)];
+  }).join('');
+
+  p.innerHTML = str;
+  iteration += 0.5;
+
 }
+p.addEventListener('mouseenter',function(){
+  iteration=0;
+  setInterval(randombanana, 30);
+});
 
 
